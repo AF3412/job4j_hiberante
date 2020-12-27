d43ad466330cafdb6de51777f6f4b1162d075cde
alex.filatov <alex.filatov@sparox.ru>
2020-12-27 17:14:29 +0300

    0. Конфигурация Hibernate [#6872]

---
+++ src/main/java/ru/af3412/Item.java
@@ -0,0 +1,61 @@
+package ru.af3412;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "items")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(id, item.id) &&
+                Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "{id='%d', name='%s'}".formatted(id, name);
+    }
+}
