80e6623d75c0265c3e0e4ca623c1b934e3f12698
alex.filatov <alex.filatov@sparox.ru>
2020-12-27 19:06:04 +0300

    1. HbmTracker. [#287705]

---
+++ src/main/java/ru/af3412/Store.java
@@ -0,0 +1,17 @@
+package ru.af3412;
+
+import java.util.List;
+
+public interface Store {
+    Item add(Item item);
+
+    boolean replace(String id, Item item);
+
+    boolean delete(int id);
+
+    List<Item> findAll();
+
+    List<Item> findByName(String key);
+
+    Item findById(int id);
+}
