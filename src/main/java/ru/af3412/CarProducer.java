bb5e9ef0015beb33dddd11ae54d746e03465aa2b
alex.filatov <alex.filatov@sparox.ru>
2021-03-23 20:53:14 +0300

    1. ToMany[#301848#504469]

---
+++ src/main/java/ru/af3412/CarProducer.java
@@ -0,0 +1,53 @@
+package ru.af3412;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "car_producer")
+public class CarProducer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CarModel> models = new ArrayList<>();
+
+    public CarProducer() {
+    }
+
+    public CarProducer(String name) {
+        this.name = name;
+    }
+
+    public void addCarModel(CarModel model) {
+        this.models.add(model);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<CarModel> getModels() {
+        return models;
+    }
+
+    public void setModels(List<CarModel> models) {
+        this.models = models;
+    }
+
+}
--- src/main/java/ru/af3412/HbmRun.java
+++ src/main/java/ru/af3412/HbmRun.java
@@ -6,8 +6,6 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import java.sql.Timestamp;
-
 public class HbmRun {
     public static void main(String[] args) {
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
@@ -18,8 +16,24 @@ public class HbmRun {
                     .buildSessionFactory();
             try (Session session = sf.openSession()) {
                 session.beginTransaction();
-                Car car = Car.of("Toyota", new Timestamp(System.currentTimeMillis()), "Sidorov Ivan");
-                session.save(car);
+                CarModel modelOne = new CarModel("one");
+                session.save(modelOne);
+                CarModel modelTwo = new CarModel("two");
+                session.save(modelTwo);
+                CarModel modelThree = new CarModel("three");
+                session.save(modelThree);
+                CarModel modelFour = new CarModel("four");
+                session.save(modelFour);
+                CarModel modelFive = new CarModel("five");
+                session.save(modelFive);
+                CarProducer carProducer = new CarProducer("Producer");
+                carProducer.addCarModel(session.load(CarModel.class, 1));
+                carProducer.addCarModel(session.load(CarModel.class, 2));
+                carProducer.addCarModel(session.load(CarModel.class, 3));
+                carProducer.addCarModel(session.load(CarModel.class, 4));
+                carProducer.addCarModel(session.load(CarModel.class, 5));
+                session.save(carProducer);
+
                 session.getTransaction().commit();
             }
         } catch (Exception e) {
